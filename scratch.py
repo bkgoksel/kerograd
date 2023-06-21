2be2f29d9939585f0a6f0545c2076501e6023c04
Kerem Goksel <bkgoksel@gmail.com>
2023-06-21 16:31:58 -0700

    small

--- scratch.py
+++ scratch.py
@@ -3,7 +3,7 @@ from computation import ComputationGraph
 from neural import *        
 
 
-epochs = 1000
+epochs = 2000
 input_d = 3
 layer_d = 5
 batch_size = 4
@@ -13,13 +13,13 @@ y = np.array(((0,1),(0,1),(1,0),(1,0)))
 l1 = LinearLayer(input_dim=input_d, output_dim=layer_d, name="L1")
 l2 = LinearLayer(input_dim=layer_d, output_dim=2, name="L2")
 
-def neural_net(x: np.ndarray, y: np.ndarray) -> np.ndarray:
-    return mean_squared_loss(l2.apply(relu(l1.apply(x), "ReLU")), y, "MSE")
+def neural_net(x: np.ndarray) -> np.ndarray:
+    return l2.apply(relu(l1.apply(x), "ReLU"))
 
 for i in range(epochs):
-    loss = neural_net(x, y)
+    loss = mean_squared_loss(neural_net(x), y, "mean_squared_loss")
     SimpleOptimizer.optimize(loss, learning_rate=2e-3)
     if not (i % 100):
         print(f"Loss: {loss}")
 
-print(ComputationGraph.from_param(neural_net(x, y), store_full_graph=True))
+#print(ComputationGraph.from_param(mean_squared_loss(neural_net(x), y, "loss"), store_full_graph=True))
