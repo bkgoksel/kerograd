2ac75ef23b236b4b33f9ca7d6cc8a125d96784bd
Kerem Goksel <bkgoksel@gmail.com>
2023-11-22 09:32:38 -0800

    button up

--- README.md
+++ README.md
@@ -1,8 +1,22 @@
 # Kerograd
 
 My own little backpropagation library, built as a personal exercise.
+Also has a GPT-style decoder-only Transformer built with it as an example.
 
-* `computation.py` for computation graph innard code.
-* `derivation.py` for gradient computation code.
-* `neural.py` for implementations of basic neural net modules using the above.
-* `scratch.py` for testing code.
+* `computation.py` for computation graph implementation. This is where computation provenance is stored to make sure gradients are propagated during backprop.
+* `derivation.py` for gradient computation code. This is where the partial derivatives for various functions are implemented.
+* `scratch.py` for testing code that overfits a tiny MLP to a single example.
+* `gpt.py` file that trains a GPT model.
+* `neural`: implementation of higher-level neural net building blocks:
+  * `attention.py`: Multi-head attention implementation
+  * `base.py`: Base utilities for making sure all these neural ops construct debuggable computation graphs.
+  * `embedding.py`: Basic embedding layer.
+  * `initialization.py`: Weight initialization algorithms. He and uniform random initialization supported.
+  * `linear.py`: Implementation of a fully connected linear layer.
+  * `loss.py`: Loss computations.Acurrently only mean squared loss is implemented. 
+  * `nets.py`: Multi-layer perceptron (MLP) implementation.
+  * `nonlinearity.py`: Nonlinearities gfor neural nets. Only ReLU implemented for now.
+  * `ops.py`: One-off operations: softmax etc.
+  * `optimizer.py`: Optimizers. Only a simple optimizer is implemented.
+  * `positional_encoding.py`: Sin/cos-based positional encoding generation as in the original transformer.
+  * `transformer.py`: The actual transformer implementation.
--- attention_net.py
+++ attention_net.py
@@ -9,26 +9,38 @@ from neural import (
     Transformer,
 )
 
+# epochs to train on
 epochs = 1000
 
+# vocab is ascii lowercase alphabet and EOS (~) character
 vocab = {"~": 0} | {t[1]: t[0] + 1 for t in enumerate(string.ascii_lowercase)}
+
+# model parameters
 model_dim = 128
 num_heads = 4
 num_decoder_layers = 1
+
+# build the model
 transformer = Transformer(vocab, model_dim, num_heads, num_decoder_layers)
 
-input_tokens = ["a", "b", "c", "d"]
-gold_output_tokens = input_tokens[1:] + ["~"]
-gold_output_indices = np.array([vocab[char] for char in gold_output_tokens])
-gold_output_one_hot = np.eye(len(vocab))[gold_output_indices]
+# input data covers the entire alphabet, some overlap between segments
+input_data = [["a","b","c","d"], ["c","d","e","f"], ["f","g","h"], ["g","h","j","k","l"],["k","l"],["l","m","n","o"],["m","n","o","p"],["m","n","o","p","q"],["q","r","s","t"],["s","t","u","v"],["v","w","x","y","z"]]
+all_gold_output_tokens = [input_tokens[1:] + ["~"] for input_tokens in input_data]
+all_gold_output_indices = [np.array([vocab[char] for char in gold_output_tokens]) for gold_output_tokens in all_gold_output_tokens]
+all_gold_output_one_hot = [np.eye(len(vocab))[gold_output_indices] for gold_output_indices in all_gold_output_indices]
 
 def iteration(i):
-    logits = transformer.apply(input_tokens)
-    loss = mean_squared_loss(logits, gold_output_one_hot)
-    SimpleOptimizer.optimize(loss, learning_rate=2e-4, gradient_clip=1e2)
+    total_loss = .0
+    # no batching, iterate over each example one-by-one
+    for input_tokens, gold_output_one_hot in zip(input_data, all_gold_output_one_hot):
+        logits = transformer.apply(input_tokens)
+        loss = mean_squared_loss(logits, gold_output_one_hot)
+        SimpleOptimizer.optimize(loss, learning_rate=2e-4, gradient_clip=1e2)
+        total_loss = total_loss + loss
+    avg_loss = total_loss / len(input_data)
     if not (i % 4):
         prediction = ' '.join(transformer._get_prediction(logits))
-        print(f"Loss: {loss}, Prediction: {prediction}")
+        print(f"Loss: {avg_loss}, Prediction: {prediction}")
 
 for i in range(epochs):
     iteration(i)
